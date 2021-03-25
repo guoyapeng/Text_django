dca2db76a7f5da5238fb1aeb375e0580f50171e4
guoyapeng <guoyapengls@outlook.com>
2021-03-25 22:50:00 +0800

    数据库连接

--- izufang/settings.py
+++ izufang/settings.py
@@ -76,9 +76,15 @@ WSGI_APPLICATION = 'izufang.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'zufang',
+        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'CHARSET': 'utf8',
+        'TIME_ZONE': 'Asia/Shanghai',
+    },
 }
 
 
@@ -104,9 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 

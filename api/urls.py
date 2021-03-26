d48d51483e0b6e7ede09b1c2ee6190827df2c99e
guoyapeng <guoyapengls@outlook.com>
2021-03-26 10:54:41 +0800

    注释修改

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from api.views import get_provinces_1_1, get_provinces_1_2, get_provinces_2_1, get_provinces_2_2
 
 urlpatterns = [
-    # 三种试图函数接口到实现方式
+    # 视图函数的序列化方式，接口实现方式和返回方式测试
     path('districts_1_1/', get_provinces_1_1),
     path('districts_1_2/', get_provinces_1_2),
     path('districts_2_1/', get_provinces_2_1),

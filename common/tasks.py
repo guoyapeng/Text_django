53a3d62dbcd8fe21c95018a2ed9845a92b6091d5
guoyapeng <guoyapengls@outlook.com>
2021-04-02 02:02:59 +0800

    业务功能开发。利用celery+rabbitMQ实现定期垃圾数据清理。

--- common/tasks.py
+++ common/tasks.py
@@ -1,6 +1,19 @@
+from datetime import timedelta
+
+from django.db import DatabaseError
+from django.utils import timezone
+
+from common.models import Record
 from izufang import app
 
 
 @app.task
 def display_info(content):
-    print(content)
+    """定期清理过期垃圾记录"""
+    check_time = timezone.now() - timedelta(days=90)
+    try:
+        Record.objects.filter(recorddate__lte=check_time) \
+            .delete()
+        return True
+    except DatabaseError:
+        return False
